eeb5db1cc08c8ab78ef8f75c858cc6674ec80ea0
korin5 <1878464932@qq.com>
2023-02-17 20:34:20 +0800

    something

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,56 +1,19 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import { ref, watch, watchEffect } from 'vue'
+<script setup lang="ts">
+import {reactive,ref,computed} from 'vue'
+import Header from './components/Header.vue'
 
-const text = ref()
-const isRight = ref(' ')
 
-watchEffect(() => {
-    if (text.value == '123') {
-        isRight.value = 'Right!'
-    } else if (!text.value) {
-        isRight.value = ' '
-    } else isRight.value = 'It is wrong...'
-})
 </script>
 
 <template>
-
-    <div>
-        <h1>Enter 123</h1>
-        <input type="text" v-model="text">
-        <p>{{ isRight }}</p>
-    </div>
-
-    <ListComponent />
-
-    <HelloWorld msg="Hello World!">
-
-        <template #header>
-            <h2>标题</h2>
-        </template>
-        <template #main>
-            <p>内容</p>
-        </template>
-        <template #footer>
-            <h3>页脚</h3>
-        </template>
-        Click me!
-    </HelloWorld>
+  <div>
+    <Header />
+  </div>
 </template>
 
 <style scoped>
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-}
-
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
+*{
+    margin: 0;
+    padding: 0;
 }
 </style>
---
+++ vite-project/src/components/Header.vue
@@ -0,0 +1,43 @@
+<script setup lang='ts'>
+//<script setup>
+import { ref,reactive } from 'vue'
+
+</script>
+
+<template>
+
+    <div id="main">
+        <ul>
+            <li><a href="#">曲目</a></li>
+            <li><a href="#">演奏家</a></li>
+        </ul>
+    </div>
+
+    <iframe src="//player.bilibili.com/player.html?aid=723739713&bvid=BV1dS4y1y7vd&cid=887643808&page=12" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
+
+</template>
+
+<style scoped>
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    ul li{
+        display:inline-block;
+        width: 200px;
+        font-size: 30px;
+        margin: 10px;
+        background-color: rgb(83, 90, 99);
+        border-radius: 100px;
+        text-align: center;
+    }
+    
+    a{
+        text-decoration: none;
+        color: rgb(249, 252, 255);
+    }
+    #main{
+        background-color: rgb(118, 135, 152);
+        border-radius: 100px;
+    }
+</style>
